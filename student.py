ab812dcf91fad1345c4c1c306088b77c43adec62
C1oud <125601340+CadenGehrke@users.noreply.github.com>
2023-04-13 19:11:43 +0000

    added Gehrke menu

--- student.py
+++ student.py
@@ -41,6 +41,7 @@ class Piggy(PiggyParent):
                 "f": ("Follow", self.follow),
                 "c": ("Calibrate", self.calibrate),
                 "q": ("Quit", self.quit)
+                
                 }
         # loop and print the menu...
         for key in sorted(menu.keys()):
@@ -55,7 +56,8 @@ class Piggy(PiggyParent):
     STUDENT PROJECTS
     ****************
     '''
-
+    def Gehrke(self):
+      pass
     def dance(self):
         """A higher-ordered algorithm to make your robot dance"""
         # TODO: check to see if it's safe before dancing
