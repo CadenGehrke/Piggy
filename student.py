f4b14da077d545749aeda144abbba1864b065c2f
CadenGehrke <125601340+CadenGehrke@users.noreply.github.com>
2023-05-16 18:07:54 +0000

    weg

--- student.py
+++ student.py
@@ -79,21 +79,19 @@ class Piggy(PiggyParent):
           time.sleep(2)
 
         
-   def twall(self):
-     
-     while True:
-       self.fwd()
-       time.sleep()
-       if self.read_distance() < 250:
-         self.right()
-         time.sleep(0.25)
-         self.fwd()
-         time.sleep(0.5)
-         self.right()
-         time.sleep()
-         
-         
-       
+    def twall(self):
+      while True:
+        self.fwd()
+        time.sleep()
+        if self.read_distance() < 250:
+          self.right()
+          time.sleep(0.25)
+          self.fwd()
+          time.sleep(0.5)
+          self.right()
+          time.sleep()
+             
+    def twall(self):
      
 
     
